52d5176d591883745ac0d819a60d82e800a2f2c4
Ugnius <ugniusvalaitis@gmail.com>
2020-08-22 11:29:23 -0700

    added open access to API

--- app.js
+++ app.js
@@ -13,8 +13,14 @@ const count = require('./functions/count')
 const port = 3001
 const app = express();
 app.use(bodyParser.json());
-app.use(cors({ origin: '*' }));
-
+// app.use(cors({ origin: '*' }));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.get('/', async (req, res) => {
     try {
